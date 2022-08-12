99cb88998e770cf4804dad653940bf237ae5d0c2
subo8 <byambadorj.batsukh@gmail.com>
2022-08-11 22:37:36 -0500

    card added

--- React/feedback-app/src/App.js
+++ React/feedback-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList';
+import data from './data/data'
 import './App.css';
 
 function App() {
+  const [feedback, setFeedback] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   );
 }
 
